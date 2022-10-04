db3708ce836e86817ba6adcd34155a8faa744141
jabbequbs <jabbequbs@users.noreply.github.com>
2022-10-04 09:22:07 -0600

    Added color samples

--- risk.py
+++ risk.py
@@ -70,15 +70,21 @@ def get_vertices(radius):
     for face, color in zip(faces, colors):
         for strip in faces[face]:
             strip = normalized(strip)
+            colors = []
+            for i in range(0, len(strip), 3):
+                s, t = texcoords(strip[i:i+3])
+                colors.extend((255, int(255*s), int(255*s), 255))
             result.append(pyglet.graphics.vertex_list(
                 len(strip)//3,
-                ("v3f", strip), # ("c4B", color*(len(strip)//3)),
-                ("t2f", texcoords(strip))
+                ("v3f", strip),
+                # ("c4B", color*(len(strip)//3))
+                ("c4B", colors)
+                # ("t2f", texcoords(strip))
                 ))
             # TODO: fix the texture coords around the international date line
     return result
 
-def get_vertices(radius):
+def get_vertices_old(radius):
     step = 10
     vlists = []
     sphere_height = 2 # [sin(90), sin(270)]
@@ -104,18 +110,40 @@ def get_vertices(radius):
         vlists.append(vlist)
     return vlists
 
+def from_hex(color):
+    if color[0] == "#":
+        color = color[1:]
+    if len(color) != 6:
+        raise ArgumentError("hex color must be 6 characters long")
+    return tuple(int(color[i:i+2], base=16) for i in range(0, len(color), 2))
+
+
 class RiskWindow(pyglet.window.Window):
     def __init__(self):
-        super().__init__(1024, 768, caption="Risk", resizable=True)
+        self.ui_batch = []
+        self.player_colors = ["8b0000", "006400", "00008b", "008b8b", "ff8c00", "8b008b", "bdb76b", "2f4f4f"]
+        for idx, color in enumerate(self.player_colors):
+            button_width = 80
+            button_height = 40
+            button_spacing = 20
+            vertices = ("v2i", (
+                button_spacing+(idx*button_width)+(idx*button_spacing), button_spacing,
+                button_spacing+(idx*button_width)+(idx*button_spacing), button_spacing+button_height,
+                button_spacing+(idx*button_width)+(idx*button_spacing)+button_width, button_spacing,
+                button_spacing+(idx*button_width)+(idx*button_spacing)+button_width, button_spacing+button_height,))
+            colors = ("c3B", from_hex(color)*4)
+            print(vertices, colors)
+            self.ui_batch.append(pyglet.graphics.vertex_list(4, vertices, colors))
+        super().__init__(1280, 720, caption="Risk", resizable=True)
         self.fpsDisplay = pyglet.window.FPSDisplay(self)
         self.texture = pyglet.image.load('2k_earth_daymap.jpg').get_texture()
         self.sphere_height = 2
-        self.vertices = get_vertices(self.sphere_height/2)
+        self.vertices = get_vertices_old(self.sphere_height/2)
         self.tilt = 0
         self.rotation = 0
         self.screen_height = 3
         self.rotation_factor = self.sphere_height / self.screen_height * self.height / 180
-        self.maximize()
+        # self.maximize()
         # super().__init__ already created the window/OpenGL context, so init here
         glPointSize(2)
         glColor3f(1, 1, 1)
@@ -134,13 +162,20 @@ class RiskWindow(pyglet.window.Window):
         glRotatef(self.rotation, 0, 1, 0)
         glEnable(GL_TEXTURE_2D)
         glBindTexture(GL_TEXTURE_2D, self.texture.id)
-        glTexParameteri(GL_TEXTURE_2D,  GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-        glTexParameteri(GL_TEXTURE_2D,  GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
         for v in self.vertices:
             v.draw(GL_TRIANGLE_STRIP)
 
         # Draw UI stuff (overtop of the globe)
         glDisable(GL_DEPTH_TEST)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glOrtho(0, self.width, self.height, 0, -1, 1)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        for item in self.ui_batch:
+            item.draw(GL_TRIANGLE_STRIP)
         self.fpsDisplay.draw()
 
     def update(self, dt):
